13d968eb93dcec974322898da06470af1015903d
Hareesh Kodali <36131996+hareeshk95@users.noreply.github.com>
2020-11-05 02:34:25 +0530

    Update JenkinsJavaDemo.java

--- JenkinsJavaDemo.java
+++ JenkinsJavaDemo.java
@@ -1,6 +1,6 @@
 package com.hk.jenkins;
 
-public class JenkinsJavaDemo {
+public class JenkinsJava {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello..!! Welcome to Jenkins Java Demo Build");
